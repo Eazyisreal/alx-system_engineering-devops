be0cb0f7afcccfa50a0f72930e5e3faab75edb4a
Eazyisreal <eazyisreal192@gmail.com>
2023-07-04 15:15:16 +0100

    Task done

---
+++ 0x05-processes_and_signals/newline.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+directory="/alx-system_engineering-devops/0x05-processes_and_signals"  # Replace with your target directory
+
+for file in "$directory"/*; do
+    if [ -f "$file" ]; then
+        echo "" >> "$file"
+    fi
+done
+
+
